bbdbf4648e39ec7c5b31c564a0cc3fba31b47698
Alex Ling <22086776+alexling540@users.noreply.github.com>
2022-01-22 18:22:12 -0600

    more test

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,48 @@
 
 // You can delete this file if you're not using it
 
-exports.createPages = async ({ actions: { createPage }, graphql }) => {};
+exports.createPages = async ({ actions: { createPage }, graphql }) => {
+  const { createPage } = actions;
+
+  return graphql(`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              templateKey
+            }
+          }
+        }
+      }
+    }
+  `).then((result) => {
+    if (result.errors) {
+      result.errors.forEach((e) => console.error(e.toString()));
+      return Promise.reject(result.errors);
+    }
+
+    const md = result.data.allMarkdownRemark.edges;
+
+    md.forEach((edge) => {
+      const id = edge.node.id;
+      createPage({
+        path: edge.node.fields.slug,
+        component: path.resolve(
+          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+        ),
+        // additional data can be passed via context
+        context: {
+          id,
+        },
+      });
+    });
+  });
+};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
@@ -19,4 +60,14 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       value,
     });
   }
+};
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+  const typeDefs = `
+    type markdownRemark implements Node @dontInfer {
+      frontmatter
+    }
+  `;
+  createTypes(typeDefs);
 };
