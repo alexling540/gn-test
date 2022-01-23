79e9bd73248834c9bbfe3c42f1ed19cff56c0d25
Alex Ling <22086776+alexling540@users.noreply.github.com>
2022-01-22 18:27:16 -0600

    fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+const { createFilePath } = require("gatsby-source-filesystem");
 
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions;
